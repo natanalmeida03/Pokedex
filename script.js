fc2f6f30a9e95f764009552a28d99939fe886aeb
Kaio Enzo Salgado <kaioenzobr@mgmail.com>
2024-08-17 00:59:22 -0300

    feat: show message error on pokemon not found

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ const pokemonImg = document.getElementById("pokemon-img");
 
 let searchPokemon = "1";
 const requestPokemon = async (search) => {
-  const r = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
-  const json = await r.json();
-  return json;
-}
+  try {
+    const r = await fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
+    const json = await r.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
 const renderPokemon = async (p) => {
   // pokemonName.innerHTML = "carregando...";
-  pokemonImg.style.display = "none";
 
   const data = await requestPokemon(p);
+  if (data === null) {
+    alert(`Pokemon '${p}' não encontrado`);
+    return;
+  }
+  pokemonImg.style.display = "none";
 
   pokemonName.innerHTML = `${data.id} - ${data.name}`;
   pokemonImg.style.display = "block ";
   searchPokemon = data.id;
   pokemonImg.src = data.sprites.other.showdown.front_default;
-}
-
+};
 
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
@@ -30,7 +38,7 @@ input.addEventListener("keydown", (e) => {
 });
 
 function nextPokemon() {
-  console.log("aqui")
+  console.log("aqui");
   searchPokemon++;
   renderPokemon(searchPokemon);
 }
@@ -44,6 +52,3 @@ function prevPokemon() {
 }
 
 renderPokemon(searchPokemon);
-
-
-
